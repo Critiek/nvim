98e610158c9e34b3cd185251acecd54246ca16a1
Critiek <jamesvsomeren@gmail.com>
2025-02-19 19:51:34 +1030

    Init Commit

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,51 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local configs = require "nvchad.configs.lspconfig"
+
+local servers = {
+  html = {},
+  cssls = {},
+  rust_analyzer = {},
+
+  pyright = {
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          typeCheckingMode = "off",
+        },
+      },
+    },
+  },
+}
+
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+
+  require("lspconfig")[name].setup(opts)
+end
+
+-- local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+-- local servers = { "html", "cssls" }
+-- local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = nvlsp.on_attach,
+--     on_init = nvlsp.on_init,
+--     capabilities = nvlsp.capabilities,
+--   }
+-- end
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
---
+++ lua/mappings.lua
@@ -0,0 +1,20 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map({ "n", "v" }, "<C-d>", "<C-d>zz")
+map({ "n", "v" }, "<C-u>", "<C-u>zz")
+
+-- map({ "n", "v" }, "x", '"_x')
+-- map({ "n", "v" }, "X", '"_x')
+map({ "n" }, "x", '"_x')
+
+map({ "n", "v" }, "d", '"_d')
+map({ "n", "v" }, "D", '"_D')
