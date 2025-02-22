f4af820e75540e2f6bfef501236f4a2db6129307
Critiek <jamesvsomeren@gmail.com>
2025-02-22 17:12:46 +1030

    fixed bindings for nvim-tree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,4 +22,12 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      return require "configs.nvim-tree"
+    end,
+  },
 }
