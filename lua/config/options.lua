a6ff482a6b0c81a69dcda409edee0c200c60c614
James <58063875+Critiek@users.noreply.github.com>
2025-07-11 19:22:36 +0930

    spelling language, auto format on save off

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,14 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.scrolloff = 4
+local opt = vim.opt
 
-vim.opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
--- vim.opt.smartindent = false -- Disables automatic indentation
+opt.scrolloff = 4
 
--- vim.opt.clipboard = ""
+opt.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+opt.expandtab = true -- Use spaces instead of tabs
+opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
+-- opt.smartindent = false -- Disables automatic indentation
+
+-- opt.clipboard = ""
+
+opt.spelllang = { "en_au" }
 
 vim.g.snacks_animate = false
+
+vim.g.autoformat = false
